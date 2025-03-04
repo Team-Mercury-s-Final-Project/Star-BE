7c68dbb310dc1c07ffedef7e3fe0527b4a05572e
“mafatofu” <qwqw0310@gmail.com>
2025-03-04 11:22:24 +0900

    refactor : 채팅 / 채팅목록 관련 큐 이름 일부 수정

--- src/main/java/com/mercury/star_be/global/config/RabbitMQConfig.java
+++ src/main/java/com/mercury/star_be/global/config/RabbitMQConfig.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,7 +41,7 @@ public class RabbitMQConfig {
     public static final String CHAT_EXCHANGE_NAME = "chat.exchange";
     public static final String READ_CHECK_EXCHANGE_NAME = "readCheck.exchange";
 
-    public static final String ROUTING_KEY = "chat.*";
+    public static final String CHAT_ROUTING_KEY = "chat.*";
     public static final String CHAT_RECENT_MESSAGE_ROUTING_KEY = "chat.recentMessage.*";
     public static final String READ_CHECK_REQUEST_ROUTING_KEY = "readCheck.request.*";
     public static final String READ_CHECK_BULK_RESPONSE_ROUTING_KEY = "readCheck.bulkResponse.*";
@@ -53,18 +54,11 @@ public class RabbitMQConfig {
     private static final String TIMER_EXCHANGE_NAME = "groups.exchange";
     private static final String TIMER_ROUTING_KEY = "groups.#";
 
-    //Queue 등록
-    @Bean
-    public Queue chatQueue() {
-        //chat.queue라는 이름의 새로운 큐를 생성
-        return new Queue("chat.queue");
-    }
-
 
     //Queue 등록(채팅 / 메시지 읽음)
     //채팅 큐
     @Bean
-    public Queue queue(){ return new Queue(CHAT_QUEUE_NAME, true); }
+    public Queue chatQueue(){ return new Queue(CHAT_QUEUE_NAME, true); }
     //채팅목록 최신 메시지 큐
     @Bean
     public Queue recentMessageQueue() {
@@ -99,7 +93,7 @@ public class RabbitMQConfig {
 
     //Exchange 등록
     @Bean
-    public TopicExchange exchange(){ return new TopicExchange(CHAT_EXCHANGE_NAME); }
+    public TopicExchange chatExchange(){ return new TopicExchange(CHAT_EXCHANGE_NAME); }
     @Bean
     public TopicExchange readCheckExchange(){ return new TopicExchange(READ_CHECK_EXCHANGE_NAME); }
 
@@ -111,38 +105,46 @@ public class RabbitMQConfig {
 
     //Exchange와 Queue 바인딩
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public Binding sendMessageBinding(Queue chatQueue, TopicExchange chatExchange) {
+        return BindingBuilder.bind(chatQueue).to(chatExchange).with(CHAT_ROUTING_KEY);
     }
+
     @Bean
-    public Binding chatRecentMessageBinding(Queue recentMessageQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(recentMessageQueue).to(exchange).with(CHAT_RECENT_MESSAGE_ROUTING_KEY);
+    public Binding chatRecentMessageBinding(Queue recentMessageQueue, TopicExchange chatExchange) {
+        return BindingBuilder.bind(recentMessageQueue).to(chatExchange).with(CHAT_RECENT_MESSAGE_ROUTING_KEY);
     }
+
+    @Bean
+    public Binding chatConnectBinding(Queue chatConnectQueue, TopicExchange chatExchange) {
+        return BindingBuilder.bind(chatConnectQueue).to(chatExchange).with(CHAT_CONNECT_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding chatDisconnectBinding(Queue chatDisconnectQueue, TopicExchange chatExchange) {
+        return BindingBuilder.bind(chatDisconnectQueue).to(chatExchange).with(CHAT_DISCONNECT_ROUTING_KEY);
+    }
+
     @Bean
     public Binding readCheckRequestBinding(Queue readCheckRequestQueue, TopicExchange readCheckExchange) {
         return BindingBuilder.bind(readCheckRequestQueue).to(readCheckExchange).with(READ_CHECK_REQUEST_ROUTING_KEY);
     }
+
     @Bean
     public Binding readCheckBulkRequestBinding(Queue readCheckBulkResponseQueue, TopicExchange readCheckExchange) {
         return BindingBuilder.bind(readCheckBulkResponseQueue).to(readCheckExchange).with(READ_CHECK_BULK_RESPONSE_ROUTING_KEY);
     }
+
     @Bean
     public Binding readCheckResponseBinding(Queue readCheckResponseQueue, TopicExchange readCheckExchange) {
         return BindingBuilder.bind(readCheckResponseQueue).to(readCheckExchange).with(READ_CHECK_RESPONSE_ROUTING_KEY);
     }
+
     // Timer Exchange와 Queue 바인딩
     @Bean
     public Binding timerBinding(Queue timerQueue, TopicExchange timerExchange) {
         return BindingBuilder.bind(timerQueue).to(timerExchange).with(TIMER_ROUTING_KEY);
     }
-    @Bean
-    public Binding chatConnectBinding(Queue chatConnectQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(chatConnectQueue).to(exchange).with(CHAT_CONNECT_ROUTING_KEY);
-    }
-    @Bean
-    public Binding chatDisconnectBinding(Queue chatDisconnectQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(chatDisconnectQueue).to(exchange).with(CHAT_DISCONNECT_ROUTING_KEY);
-    }
+
 
     /* messageConverter를 커스터마이징 하기 위해 Bean 새로 등록 */
     @Bean
